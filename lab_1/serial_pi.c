c7bde4e752b672cde7c7ce823b85c503dbb193aa
Andrea Bonandin <andrea.bonandin@gmail.com>
2016-11-26 17:53:15 +0100

    refactoring

--- lab_1/serial_pi.c
+++ lab_1/serial_pi.c
@@ -1,16 +1,22 @@
+#include <stdio.h>
+
 static long num_steps=100000;
 double step, pi;
 
-void main()
-{  int i;
-   double x, sum = 0.0;
+int main(int argc,char *argv[])
+{
+	int i;
+	double x, sum = 0.0;
 
-   step = 1.0/(double) num_steps;
+	step = 1.0/(double) num_steps;
 
-   for (i=0; i< num_steps; i++){
-      x = (i+0.5)*step;
-      sum = sum + 4.0/(1.0 + x*x);
-   }
-   pi = step * sum;
-   printf("Pi = %f\n",pi);
+	for (i=0; i < num_steps; i++) {
+		x = (i + 0.5)*step;
+		sum = sum + 4.0/(1.0 + x*x);
+	}
+
+	pi = step * sum;
+	printf("Pi = %f\n", pi);
+
+	return 0;
 }
