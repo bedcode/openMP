9f5a480f763644d20063442b20bbce5810ddf6e2
Andrea Bonandin <andrea.bonandin@gmail.com>
2016-12-14 22:13:57 +0100

    better indentation

--- lab_1/omp_reduction.c
+++ lab_1/omp_reduction.c
@@ -4,29 +4,29 @@
 *   OpenMP Example - Combined Parallel Loop Reduction - C/C++ Version
 *   This example demonstrates a sum reduction within a combined parallel loop
 *   construct.  Notice that default data element scoping is assumed - there
-*   are no clauses specifying shared or private variables.  OpenMP will 
+*   are no clauses specifying shared or private variables.  OpenMP will
 *   automatically make loop index variables private within team threads, and
 *   global variables shared.
 ******************************************************************************/
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
+#define N 100
 
-int main (int argc, char *argv[]) 
+int main (int argc, char *argv[])
 {
-int   i, n;
-float a[100], b[100], sum; 
+	int i;
+	float a[N], b[N], sum;
 
-/* Some initializations */
-n = 100;
-for (i=0; i < n; i++)
-  a[i] = b[i] = i * 1.0;
-sum = 0.0;
+	/* Some initializations */
+	for (i=0; i < N; i++)
+		a[i] = b[i] = i * 1.0;
+	sum = 0.0;
 
-#pragma omp parallel for reduction(+:sum)
-  for (i=0; i < n; i++)
-    sum = sum + (a[i] * b[i]);
+	#pragma omp parallel for reduction(+:sum)
+		for (i=0; i < N; i++)
+			sum = sum + (a[i] * b[i]);
 
-printf("   Sum = %f\n",sum);
-
+	printf("Sum = %f\n", sum);
+	return 0;
 }
