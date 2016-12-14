8d4dbe70139acec5bd5079d691fbdb60e42f0c4f
Andrea Bonandin <andrea.bonandin@gmail.com>
2016-12-14 22:03:20 +0100

    better indentation

--- lab_1/omp_orphan.c
+++ lab_1/omp_orphan.c
@@ -14,29 +14,27 @@ float a[VECLEN], b[VECLEN], sum;
 
 float dotprod ()
 {
-int i,tid;
+	int i, tid;
 
-tid = omp_get_thread_num();
-#pragma omp for reduction(+:sum)
-  for (i=0; i < VECLEN; i++)
-    {
-    sum = sum + (a[i]*b[i]);
-    printf("  tid= %d i=%d\n",tid,i);
-    }
+	tid = omp_get_thread_num();
+	#pragma omp for reduction(+:sum)
+	for (i=0; i < VECLEN; i++) {
+		sum = sum + (a[i]*b[i]);
+		printf("  tid = %d  i = %d\n", tid, i);
+	}
 }
 
 
 int main (int argc, char *argv[]) {
-int i;
+	int i;
 
-for (i=0; i < VECLEN; i++)
-  a[i] = b[i] = 1.0 * i;
-sum = 0.0;
+	for (i=0; i < VECLEN; i++)
+		a[i] = b[i] = 1.0 * i;
+	sum = 0.0;
 
-#pragma omp parallel
-  dotprod();
+	#pragma omp parallel
+		dotprod();
 
-printf("Sum = %f\n",sum);
-
+	printf("Sum = %f\n", sum);
+	return 0;
 }
-
--- lab_1/omp_workshare1.c
+++ lab_1/omp_workshare1.c
@@ -12,35 +12,31 @@
 #define CHUNKSIZE   10
 #define N       100
 
-int main (int argc, char *argv[]) 
+int main (int argc, char *argv[])
 {
-int nthreads, tid, i, chunk;
-float a[N], b[N], c[N];
+	int nthreads, tid, i, chunk;
+	float a[N], b[N], c[N];
 
-/* Some initializations */
-for (i=0; i < N; i++)
-  a[i] = b[i] = i * 1.0;
-chunk = CHUNKSIZE;
+	/* Some initializations */
+	for (i=0; i < N; i++)
+		a[i] = b[i] = i * 1.0;
+	chunk = CHUNKSIZE;
 
-#pragma omp parallel shared(a,b,c,nthreads,chunk) private(i,tid)
-  {
-  tid = omp_get_thread_num();
-  if (tid == 0)
-    {
-    nthreads = omp_get_num_threads();
-    printf("Number of threads = %d\n", nthreads);
-    }
-  printf("Thread %d starting...\n",tid);
+	#pragma omp parallel shared(a, b, c, nthreads, chunk) private(i, tid)
+	{
+		tid = omp_get_thread_num();
+		if (tid == 0) {
+			nthreads = omp_get_num_threads();
+			printf("Number of threads = %d\n", nthreads);
+		}
+		printf("Thread %d starting...\n", tid);
 
-  #pragma omp for schedule(dynamic,chunk)
-  for (i=0; i<N; i++)
-    {
-    c[i] = a[i] + b[i];
-    printf("Thread %d: c[%d]= %f\n",tid,i,c[i]);
-    }
+		#pragma omp for schedule(dynamic, chunk)
+		for (i=0; i<N; i++) {
+			c[i] = a[i] + b[i];
+			printf("Thread %d: c[%d]= %f\n", tid, i, c[i]);
+		}
+	}  /* end of parallel section */
 
-  }  /* end of parallel section */
-
+	return 0;
 }
-
-
