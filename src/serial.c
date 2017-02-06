c5d27ad08133ec268b30ad19f686a19f92c9685b
Andrea Bonandin <andrea.bonandin@gmail.com>
2017-02-06 10:05:27 +0100

    adjustments

--- src/serial.c
+++ src/serial.c
@@ -7,6 +7,7 @@
 #define NSTEPS 6    /* number of time steps */
 #define BORDERI (2 + NI)
 #define BORDERJ (2 + NJ)
+
 void init(int *old);
 void evolve(int *old, int *new);
 void update(int *old, int *new);
@@ -20,51 +21,52 @@ void init(int *old) {
     for(i = 0; i < BORDERI; i++) {
         for(j = 0; j < BORDERJ; j++){
             if (i==0 || j==0 || i==(BORDERI-1)|| j==BORDERJ-1)
-                old[i*(BORDERJ)+j]=0;
+                old[i*(BORDERJ) + j]=0;
             else {
                 x = rand()/((float)RAND_MAX + 1);
                 if(x < 0.5) {
-                    old[i*(BORDERJ)+j] = 0;
+                    old[i*(BORDERJ) + j] = 0;
                 } else {
-                    old[i*(BORDERJ)+j] = 1;
+                    old[i*(BORDERJ) + j] = 1;
                 }
             }
         }
     }
-    
 }
 
 /* change from old state to new state */
 void evolve(int *old, int *new) {
-    int i, j, iu, id, jl, jr, nsum= 0;
-    
+    int i, j, iu, id, jl, jr, nsum = 0;
+
     /* corner boundary conditions */
-    old[0] = old[(NI)*(BORDERJ) + (NJ)];
-    old[BORDERJ-1] = old[NI*(BORDERJ) + 1];
-    old[(BORDERI-1)*(BORDERJ) + BORDERJ-1] = old[(BORDERJ) + 1];
-    old[(BORDERI-1)*(BORDERJ)] = old[1* (BORDERJ)+ NJ];
+    old[0] = old[NI*BORDERJ + NJ];
+    old[BORDERJ-1] = old[NI*BORDERJ + 1];
+    old[(BORDERI-1)*(BORDERJ) + BORDERJ-1] = old[BORDERJ + 1];
+    old[(BORDERI-1)*(BORDERJ)] = old[BORDERJ + NJ];
 
     /* left-right boundary conditions */
     for(i = 1; i < BORDERI-1; i++) {
-            old[i*(BORDERJ)] = old[i*(BORDERJ)+ NJ];
-            old[i*(BORDERJ)+BORDERJ-1] = old[i*(BORDERJ) + 1];
+        old[i*(BORDERJ)] = old[i*(BORDERJ) + NJ];
+        old[i*(BORDERJ) + BORDERJ-1] = old[i*(BORDERJ) + 1];
     }
 
     /* top-bottom boundary conditions */
-    for(j = 1; j <BORDERJ-1; j++){
-        old[j] = old[NI*(BORDERJ) + j];
-        old[(BORDERI-1)*(BORDERJ) + j] = old[((BORDERJ) +j)];
+    for(j = 1; j < BORDERJ-1; j++){
+        old[j] = old[NI*BORDERJ + j];
+        old[(BORDERI-1)*(BORDERJ) + j] = old[BORDERJ + j];
     }
-    //show(old);
-    for(i = 1; i < BORDERI -1; i++) {
-    	for(j = 1; j < BORDERJ-1; j++){
-            iu = i-1;		//up
-            id = i+1;		//down
-            jl = j-1;		//left
-            jr = j+1;		//right
 
-            nsum = old[iu * (BORDERJ) +jl] + old[iu * (BORDERJ) +j]	+ old[iu * (BORDERJ) +jr] + old[i * (BORDERJ) +jl] + old[i * (BORDERJ) +jr] + old[id * (BORDERJ) +jl] + old[id * (BORDERJ) +j] + old[id * (BORDERJ) +jr];
-            //printf("i: %d j:%d sum :%d \n ", i , j, nsum );
+    //show(old);
+
+    for(i = 1; i < BORDERI-1; i++) {
+        for(j = 1; j < BORDERJ-1; j++){
+            iu = i-1;   //up
+            id = i+1;   //down
+            jl = j-1;   //left
+            jr = j+1;   //right
+
+            nsum = old[iu * (BORDERJ) +jl] + old[iu * (BORDERJ) +j] + old[iu * (BORDERJ) +jr] + old[i * (BORDERJ) +jl] + old[i * (BORDERJ) +jr] + old[id * (BORDERJ) +jl] + old[id * (BORDERJ) +j] + old[id * (BORDERJ) +jr];
+            //printf("i: %d j:%d sum :%d \n ", i , j, nsum);
             if (nsum == 3) {
                 new[i*(BORDERJ) + j] = 1;
                 nsum = 0;
@@ -95,11 +97,12 @@ void show(int *array) {
     int i, j;
 
     for(i = 0; i < BORDERI; i++) {
-        for(j = 0; j <BORDERJ; j++) {
+        for(j = 0; j < BORDERJ; j++) {
             printf("%d", array[i*(BORDERJ) + j]);
         }
         printf("\n");
     }
+    printf("\n");
 }
 
 int main(int argc, char *argv[]) {
@@ -111,14 +114,13 @@ int main(int argc, char *argv[]) {
     /* allocate arrays */
     ni = BORDERI;  /* add 2 for left and right ghost cells */
     nj = BORDERJ;
-    old = malloc(ni*nj*sizeof(int));
-    new = malloc(ni*nj*sizeof(int));
+    old = malloc(BORDERI * BORDERJ * sizeof(int));
+    new = malloc(BORDERI * BORDERJ * sizeof(int));
 
     init(old);
     show(old);
-    printf("\n\n");
+
     for (n = 0; n < NSTEPS; n++) {
-      	
         evolve(old, new);
         update(old, new);
     }
